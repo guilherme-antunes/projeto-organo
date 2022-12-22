8f3a05c4fd9086dffb3275f51041ac45324665b1
guilherme-antunes <112368848+guilherme-antunes@users.noreply.github.com>
2022-12-22 16:05:24 -0300

    Atualizado

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -26,7 +26,7 @@ const Rodape = () => {
         </section>
         <section>
             <p>
-                Desenvolvido por Alura.
+                Desenvolvido por Guilherme H Antunes.
             </p>
         </section>
     </footer>)
